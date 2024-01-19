f2d85366a52c6c089950f778abf06a7577af7faf
Surajdhage31 <surajdhage2006@gmail.com>
2024-01-19 22:45:30 +0530

    this is reverse String program

--- src/Book_Ticket/Login.java
+++ src/Book_Ticket/Login.java
@@ -1,14 +1,11 @@
 package Book_Ticket;
 import java.util.Scanner;
 public class Login {
-
+//static String revStr(String S)
 		public static void main(String[] args) {
-			
-		
 				  Scanner a = new Scanner(System.in);
 				System.out.println("Welcome , pleses login your account");
 				System.out.println("enter your username");
-				
 				String c = a.next();
 				System.out.println("Enter your password");
 				//Scanner b = new Scanner(System.in);
@@ -18,18 +15,26 @@ public class Login {
 				if ( (c.equalsIgnoreCase(user)) && (pass == d ))
 				{
 					System.out.println("login Successfull");
+					
+					System.out.println("Enter you'r choice");
+					System.out.println(" 1. Book Ticket");
+					
+					Scanner put = new Scanner (System.in);
+					int book = put.nextInt();
+					switch(book)
+					{
+					case 1 :
+					
+						System.out.println(" pune to mumbai");
+						System.out.println(" mumbai to pune");
+						Scanner yash = new Scanner (System.in);
+						  String sham = put.next();
+						System.out.println("Ticket book "+sham);
+					}
 				}
 				else
 				{
 					System.out.println(" Incorrect credential");
-				}
-					
-				
-				
+				}	
 			}
-			
-
 		}
-
-
-
---
+++ src/Book_Ticket/reverString.java
@@ -0,0 +1,16 @@
+
+package Book_Ticket; 
+import java.util.Scanner;
+
+public class reverString {
+
+	public static void main(String[] args) {
+	 System.out.println("enter any sentence");
+	 Scanner a = new Scanner(System.in);
+	 public revStr(String a) 
+   System.out.println();
+	 int a = 2;
+	 
+	}
+
+}
