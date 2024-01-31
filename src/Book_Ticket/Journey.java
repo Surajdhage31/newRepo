dd35a031c31aad74ca059e973d88db3944891d87
Surajdhage31 <surajdhage2006@gmail.com>
2024-01-31 14:51:23 +0530

    fixrd ticket price

--- src/Book_Ticket/Journey.java
+++ src/Book_Ticket/Journey.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Journey {
 	public int city () {
 		System.out.println(" 1.pune_to_mumbai");
-		  System.out.println("2.mumbai_to_pune");
+	    System.out.println("2.mumbai_to_pune");
 		  Scanner a = new Scanner(System.in);
 		  int bus =  a.nextInt();
-			  if( bus ==1)
+			 if( bus ==1)
 			  {
 				  System.out.println("pune_to_mumbai  ");
 				 System.out.println(" Time  12:30");
@@ -17,7 +17,7 @@ public class Journey {
 				  System.out.println("mumbai_to_pune");
 				  System.out.println("Time 3:20 ");
 			  }
-			  System.out.println("1.Sleeper \n 2.Sami-Sleeper \n 3. Seter");
+			  System.out.println("1.Sleeper \n 2.Semi-Sleeper \n 3. Seater");
 			  int red = a.nextInt();
 			  switch (red)
 			  {
@@ -25,20 +25,44 @@ public class Journey {
 			  {
 				  System.out.println("please Paid ticket price/fee Rs:400");
 				  int money= a.nextInt();
+				  if (money == 400)
+				  {
 				  System.out.println("paid :" +money);
+				  }
+				  else
+				  {
+					  System.out.println("not enough fund");
+				  }
                  break ;
 				  }
 			  case 2 :
 			  {
 				  System.out.println("please Paid ticket price/fee Rs:300 ");
 				  int money= a.nextInt();
+				  
+				  if (money == 300)
+				  {
 				  System.out.println("paid :" +money);
+				  }
+				  else
+				  {
+					  System.out.println("not enough fund");
+				  }
 				  break ;
 			  }
 			  case 3:
 			  {
 				  System.out.println("please Paid ticket price/fee Rs:210 ");
 				  int money= a.nextInt();
+				  
+				  if (money == 210)
+				  {
+				  System.out.println("paid :" +money);
+				  }
+				  else
+				  {
+					  System.out.println("not enough fund");
+				  }
 				  System.out.println("paid :" +money);
 				  
 			  }
